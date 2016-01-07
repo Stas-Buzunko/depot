6f496800abe0dfa3ec16811ce1b0d5ad6f0bb6f7
Stas-Buzunko <buzunkostas@gmail.com>
2016-01-07 15:45:15 +0300

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Product.delete_all
+# . . .
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+description:
+%{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>},
+image_url: 'cs.jpg',
+price: 49.95)
